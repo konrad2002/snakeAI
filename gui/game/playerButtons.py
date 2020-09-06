c9e90db2f9424da22b5994980fa757d356264522
konrad2002 <konrad@schwimmteamerzgebirge.de>
2020-09-06 19:48:19 +0200

    destroy buttons but prepare for floating design

--- gui/game/playerButtons.py
+++ gui/game/playerButtons.py
@@ -1,10 +1,11 @@
 # buttons for game with player slot
 
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.graphics import Rectangle,Canvas,Ellipse,Color,Line
 
-class PlayerButtons (FloatLayout):
+class PlayerButtons (RelativeLayout):
     def __init__ (self, game, **kwargs):
         super(PlayerButtons, self).__init__(**kwargs)
         print("[game] created " + str(self.__class__))
@@ -18,33 +19,39 @@ class PlayerButtons (FloatLayout):
             Color(.1, .2, .7, .5)
             self.bg = Rectangle(pos=(0, 0), size=(self.size[0], self.size[1]))
 
+            self.buttonLayer = BoxLayout(padding = 20, orientation = "vertical")
+            self.buttonLayer.size = (self.size[0], self.size[1] - 40)
+            self.buttonLayer.pos = (0, 0)
+
             self.readyBtn = Button(
                 text = "Bereit",
-                pos = (0 , 0),
-                size_hint = (.1, .1),
+                # pos = (0 , 0),
+                # size_hint = (.1, .1),
                 on_release = self.onReady
             )
 
-            self.add_widget(self.readyBtn)
+            self.buttonLayer.add_widget(self.readyBtn)
 
 
             self.key1Btn = Button(
                 text = "W-A-S-D",
                 id = "1",
-                pos = (100 , 0),
-                size_hint = (.1, .1),
+                # pos = (100 , 0),
+                # size_hint = (.1, .1),
                 on_release = self.onSetKeyboard
             )
             self.key2Btn = Button(
                 text = "Pfeiltasten",
                 id = "2",
-                pos = (200 , 0),
-                size_hint = (.1, .1),
+                # pos = (200 , 0),
+                # size_hint = (.1, .1),
                 on_release = self.onSetKeyboard
             )
 
-            self.add_widget(self.key1Btn)
-            self.add_widget(self.key2Btn)
+            self.buttonLayer.add_widget(self.key1Btn)
+            self.buttonLayer.add_widget(self.key2Btn)
+
+            self.add_widget(self.buttonLayer)
 
     def onReady(self, a):
         self.game.ready()
