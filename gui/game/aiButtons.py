8f192d40d31c5bc5bdd73bc0f99125250421b8b1
konrad2002 <konrad@schwimmteamerzgebirge.de>
2020-09-09 22:57:16 +0200

    overtook buttons from player to ai

--- gui/game/aiButtons.py
+++ gui/game/aiButtons.py
@@ -5,6 +5,8 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.graphics import Rectangle,Canvas,Ellipse,Color,Line
 
+import math
+
 class AiButtons (RelativeLayout):
     def __init__ (self, game, **kwargs):
         super(AiButtons, self).__init__(**kwargs)
@@ -15,9 +17,14 @@ class AiButtons (RelativeLayout):
     def drawBackground(self):
         with self.canvas:
             self.canvas.clear()
+            
+            self.sizeX = self.size[0]
+            self.sizeY = self.size[1]
 
             Color(.1, .2, .7, .5)
             self.bg = Rectangle(pos=(0, 0), size=(self.size[0], self.size[1]))
+
+            self.buttons = []
 
             self.readyBtn = Button(
                 text = "Bereit",
@@ -26,7 +33,36 @@ class AiButtons (RelativeLayout):
                 on_release = self.onReady
             )
 
+            self.buttons.append(self.readyBtn)
+
             self.add_widget(self.readyBtn)
+
+            
+            # calculation of button positions and size (depending on number of buttons)
+            self.sqrt = math.sqrt(len(self.buttons))
+            self.cols = round(self.sqrt + 0.4999)
+
+            for i,button in enumerate(self.buttons):
+                if self.game.main.settings.playerMode != 2:
+                    button.size_hint = (
+                        0.1,
+                        ( 1 / self.cols - ( 0.15 / self.cols ) )
+                    )
+                else:
+                    button.size_hint = (
+                        1 / self.cols,
+                        0.1
+                    )
+
+                row = round( ( i / self.cols ) + 0.4999 )
+                col = i % self.cols
+
+                button.pos = (
+                    col * self.sizeX / self.cols,
+                    row * self.sizeY / self.cols,
+                )
+
+                print("Button " + str(i) + ": " + str(col) + " | " + str(row))
 
     
     def onReady(self, a):
