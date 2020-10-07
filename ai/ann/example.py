01eed41444275be7e0d51fd2cdd43dff7c60e48b
konrad2002 <konrad@schwimmteamerzgebirge.de>
2020-10-07 18:30:08 +0200

    implement bias and custom weights

--- ai/ann/example.py
+++ ai/ann/example.py
@@ -4,7 +4,13 @@ import numpy as np
 from sklearn.utils.validation import check_random_state
 
 class AiAnnExample (object):
-    def __init__ (self, nNeurons = [1, 1, 1], weights = None, activationF = "heaviside", outputF = "id"):
+    def __init__ (  self,
+                    nNeurons = [1, 1, 1],
+                    weights = None,
+                    activationF = ["sigmoid"],
+                    outputF = ["id"]
+                ):
+
         print("[ai  ] created " + str(self.__class__))
 
         self.nNeurons = nNeurons
@@ -24,18 +30,23 @@ class AiAnnExample (object):
 
             # init neurons of layer with 0
             self.layers.append(
-                np.zeros((neurons, 5))
+                np.zeros((neurons + 1, 5))
             )
 
+            self.layers[layer][0,:] = 1
 
-            if weights:
-                self.weights.append(weights[layer])
-            else:
+
+            if not weights:
                 # init weights between layer and next layer with 0
                 if layer < ( len(self.nNeurons) - 1 ):
                     self.weights.append(
-                        self.random_state_.random_sample((neurons, self.nNeurons[layer + 1]))
+                        #self.random_state_.random_sample((neurons + 1, self.nNeurons[layer + 1] + 1))
+                        np.ones((neurons + 1, self.nNeurons[layer + 1] + 1))
                     )
+
+        
+        if weights:
+            self.weights = weights
 
     def function(self, fType, x):
 
@@ -48,7 +59,7 @@ class AiAnnExample (object):
 
         if fType == "heaviside":
             for X in x:
-                if X > 0:
+                if X >= 0:
                     y.append(1)
                 else:
                     y.append(0)
@@ -68,17 +79,27 @@ class AiAnnExample (object):
 
     def predict(self, x):
 
-        self.layers[0][:,2] = x
+        self.layers[0][1:,2] = x
 
         for i,layer in enumerate(self.layers):
             if i > 0:
-                layer[:,0] = np.dot(self.layers[i - 1][:,2], self.weights[i - 1])
+                layer[1:,0] = np.dot(self.layers[i - 1][:,2], self.weights[i - 1][:,1:])
 
-                layer[:,1] = self.function(self.activationF, layer[:,0])
+                # print("")
+                # print(self.layers[i - 1][:,2])
+                # print("dot")
+                # print(self.weights[i - 1][:,1:])
+                # print("equals")
+                # print(layer[1:,0])
+                # print("")
 
-                layer[:,2] = self.function(self.outputF, layer[:,1])
+                layer[1:,1] = self.function(self.activationF[i - 1], layer[1:,0])
+
+                layer[1:,2] = self.function(self.outputF[i - 1], layer[1:,1])
             
         output = self.layers[len(self.nNeurons) - 1][:,2]
+
+        print(str(x) + " => " + str(output))
 
         return output
 
@@ -107,6 +128,10 @@ class AiAnnExample (object):
 
 
     def printNetwork(self):
+
+        print("weights")
+        print(self.weights)
+        print("")
 
         for i,layer in enumerate(self.layers):
             print("[Layer  " + str(i) + "]: ")
@@ -137,18 +162,31 @@ class AiAnnExample (object):
                 print(" " + str(self.nNeurons[i]) + "x" + str(self.nNeurons[i + 1]) + " weights")
 
 
+# weights = []
 
-ai = AiAnnExample([12, 8, 4], None, "sigmoid")
-ai.printNetwork()
+# weights.append(
+#     np.matrix(
+#         [
+#             [0,-2],
+#             [0,1],
+#             [0,1]
+#         ]
+#     )
+# )
 
-x = [3,12,1,4,7,3,6,3,6,5,4,18]
-output = ai.predict(x)
 
-print("")
-print("")
-print("output:")
-print(output)
-print("")
-print("")
+# ai = AiAnnExample([2, 1], weights=weights, activationF=["heaviside","heaviside"], outputF=["id", "id"])
+# ai.printNetwork()
 
-ai.printNetwork()
+# X = [[0, 0], [0, 1], [1, 0], [1, 1]]
+# for x in X:
+#     output = ai.predict(x)
+
+# print("")
+# print("")
+# print("output:")
+# print(output)
+# print("")
+# print("")
+
+# ai.printNetwork()
